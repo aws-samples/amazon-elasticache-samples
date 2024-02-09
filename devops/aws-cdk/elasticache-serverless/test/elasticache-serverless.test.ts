e9b112f7a68e1bc95d9195876efde8215348227d
Roberto Luna Rojas <roberto.luna.rojas@gmail.com>
2024-02-08 20:10:23 -0500

    Amazon ElastiCache Serverless | CDK | Vpc creation

---
+++ devops/aws-cdk/elasticache-serverless/test/elasticache-serverless.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ElasticacheServerless from '../lib/elasticache-serverless-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/elasticache-serverless-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ElasticacheServerless.ElasticacheServerlessStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
