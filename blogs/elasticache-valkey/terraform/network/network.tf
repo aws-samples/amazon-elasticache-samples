09b1ca49961e30434fa30f434294e9cf27e5d342
Sourav Kundu <kunduso@amazon.com>
2025-05-03 11:35:27 -0500

    route table to subnet association

--- blogs/elasticache-valkey/terraform/network/network.tf
+++ blogs/elasticache-valkey/terraform/network/network.tf
@@ -21,3 +21,9 @@ resource "aws_route_table" "private" {
   vpc_id = aws_vpc.this.id
   tags   = merge(var.tags, { "Name" = "${var.vpc_name}-private-${count.index + 1}" })
 }
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table_association
+resource "aws_route_table_association" "private" {
+  count          = length(var.subnet_cidr_private) > 0 ? length(var.subnet_cidr_private) : 0
+  subnet_id      = element(aws_subnet.private.*.id, count.index)
+  route_table_id = aws_route_table.private[count.index].id
+}
