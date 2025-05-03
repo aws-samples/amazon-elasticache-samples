6bd907fc4e63e39ed88b2f8979f846c08bb6ac64
Sourav Kundu <kunduso@amazon.com>
2025-05-03 12:41:23 -0500

    update resource reference

--- blogs/elasticache-valkey/terraform/serverless/kms.tf
+++ blogs/elasticache-valkey/terraform/serverless/kms.tf
@@ -4,20 +4,20 @@ locals {
 }
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/kms_key
 resource "aws_kms_key" "encrypt_cache" {
-  description             = "KMS key for ${var.name}"
+  description             = "KMS key to encrypt cache for ${var.name}"
   deletion_window_in_days = 7
   enable_key_rotation     = true
 }
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/kms_alias
 resource "aws_kms_alias" "key" {
-  name          = "alias/${var.name}"
+  name          = "alias/cache-${var.name}"
   target_key_id = aws_kms_key.encrypt_cache.id
 }
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/kms_key_policy
-resource "aws_kms_key_policy" "encrypt_app" {
+resource "aws_kms_key_policy" "encrypt_policy" {
   key_id = aws_kms_key.encrypt_cache.id
   policy = jsonencode({
-    Id = "encryption-rest"
+    Id = "encrypt-serverless-cache"
     Statement = [
       {
         Action = "kms:*"
