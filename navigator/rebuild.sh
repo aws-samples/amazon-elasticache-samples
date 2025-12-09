75d6a1f4710542ace7628dc6099bd12e89f7c753
Norbert Funke <funknor@amazon.com>
2025-12-09 08:45:28 -0500

    fixed lib folder issue

--- navigator/rebuild.sh
+++ navigator/rebuild.sh
@@ -2,5 +2,9 @@
 docker-compose down
 docker image rm elasticache-navigator-frontend
 docker image rm elasticache-navigator-backend
+
+# Repo Fix as folders 'lib' are in .gitignore
+cp -r frontend/src/src_lib frontend/src/lib
+
 docker-compose build
 docker-compose up
